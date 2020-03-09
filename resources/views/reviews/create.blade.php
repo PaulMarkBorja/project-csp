a201ad2de845055feed3011a91dafe70d4238217
john marson vida <johnmarson.vida@unc.edu.ph>
2020-03-10 02:27:50 +0800

    Added views templates for Review

---
+++ resources/views/reviews/create.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('title', 'Add Review')
+
+@section('content')
+
+<h1>Create review</h1>
+
+@endsection
---
+++ resources/views/reviews/edit.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('title', 'Edit Review')
+
+@section('content')
+
+<h1>Edit review</h1>
+
+@endsection
